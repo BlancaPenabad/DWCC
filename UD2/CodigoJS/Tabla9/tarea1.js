a5a637e215178b1d5b0f70d8c2205efa54b12f23
[BlancaPenabad] <[a22blancapv@iessanclemente.net]>
2023-11-08 20:20:32 +0100

    Ejercicios UD2 Tabla9

---
+++ UD2/CodigoJS/Tabla9/tarea1.js
@@ -0,0 +1,17 @@
+/* Realizar unha pequena aplicación en JavaScript e outra en Typescript que amose (por consola ou
+por pantalla, o que che resulte máis doado) o seguinte:
+• Táboa de multiplicar do 9.
+• 10 primeiros números da sucesión de Fibonacci.
+• Indicar se un número é primo ou non. A entrada de datos podes facela como queiras
+(prompt, fixa,...)
+Utilizar os tres tipos de bucles que se viron nos contidos. Podes escoller o que che resulte máis
+axeitado para cada punto pero deberán ser diferentes, non repetir en ningún caso. */
+console.log("TABLA DEL 9: ")
+let i=0;
+while (i<=10){
+    console.log("9 por "+i+" = "+(9*i));
+    i++;
+}
+
+//METER EL CÓDIGO DENTRO DE UN HTML SIEMPRE, LUEGO SE VISUALIZAN DESDE LA CONSOLA DEL NAVEGADOR (INSPECCIONAR)
+//ls para ver contenido de un directorio y se autocompleta con tab
