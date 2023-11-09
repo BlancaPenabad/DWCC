51051ac3373e529946834667c22bab1d181d5e91
[BlancaPenabad] <[a22blancapv@iessanclemente.net]>
2023-11-09 11:54:57 +0100

    Números primos TS

---
+++ UD2/CodigoTS/Primo/primo.ts
@@ -0,0 +1,15 @@
+function isPrime(n:number):void{
+    if(n==0 || n ==1 || n==4){
+        return console.log("No es primo");
+    }
+
+    for(let x = 2; x < n / 2 ; x++){
+        if(n% x == 0)
+        return console.log("No es primo");
+
+    }
+    return console.log("Es primo.");
+}
+
+let numero:number = parseInt(prompt("Escribe un número:"));
+isPrime(numero);
