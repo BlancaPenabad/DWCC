2d39b1278cbf34afe74e154d13d90d4a2acdd545
[BlancaPenabad] <[a22blancapv@iessanclemente.net]>
2023-11-11 22:43:42 +0100

    Ejercicio edad con Typescript

---
+++ UD2/CodigoTS/Edad/edadCASE.js
@@ -0,0 +1,30 @@
+/*
+Crea un programa que pida ao usuario que introduza unha idade e amose a seguinte mensaxe en
+función do número introducido:
+• 0-12: neno
+• 13-18: adolescente
+• 19-30: xoven
+• 31-64: adulto
+• 65-100: xubilado
+Controla os casos de erro tamén.
+*/
+var tuEdad = parseInt(prompt("Escribe tu edad:"));
+switch (true) {
+    case (tuEdad >= 0 && tuEdad <= 12):
+        console.log("Neno");
+        break;
+    case (tuEdad >= 13 && tuEdad <= 18):
+        console.log("Adolescente");
+        break;
+    case (tuEdad >= 19 && tuEdad <= 30):
+        console.log("Xoven");
+        break;
+    case (tuEdad >= 31 && tuEdad <= 64):
+        console.log("Adulto");
+        break;
+    case (tuEdad >= 65 && tuEdad <= 100):
+        console.log("Xubilado");
+        break;
+    default:
+        console.log("Valor incorrecto, por favor introduce un número del 0 al 100.");
+}
---
+++ UD2/CodigoTS/Edad/edadIF.ts
@@ -0,0 +1,29 @@
+/* 
+Crea un programa que pida ao usuario que introduza unha idade e amose a seguinte mensaxe en
+función do número introducido:
+• 0-12: neno
+• 13-18: adolescente
+• 19-30: xoven
+• 31-64: adulto
+• 65-100: xubilado
+Controla os casos de erro tamén.
+*/
+
+function age(e:number):void{
+    if(e>=0 && e<=12){
+        return console.log("Neno");
+    }else if (e>=13 && e<=18){
+        return console.log("Adolescente");
+    }else if (e>=19 && e<=30){
+        return console.log("Xoven");
+    }else if (e>=31 && e<=64){
+        return console.log("Adulto");
+    }else if (e>=65 && e<=100){
+        return console.log("Xubilado");
+    }else{
+        return console.log("Número incorrecto, por favor introduce un número del 0 al 100.");
+    }
+}
+
+let years:number | null = parseInt(prompt("Escribe tu edad:"));
+age(years);
