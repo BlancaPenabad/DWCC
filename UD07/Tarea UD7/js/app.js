b6909d4268794e2688776fcc1ea50fa7e11b8027
Blanca Penabad Villar <a22blancapv@iessanclemente.net>
2024-05-28 16:24:38 +0200

    Tarea 07

---
+++ UD07/Tarea UD7/js/app.js
@@ -0,0 +1,115 @@
+// Código de validaciones del formulario
+let patronNombre = /^[a-zA-ZáéíóúÁÉÍÓÚ\s]{1,20}$/;
+let patronApellido= /^[a-zA-ZáéíóúÁÉÍÓÚ\s]{1,40}$/;
+let patronCorreo = /^[\w._%+-]+@[\w._%+-]+\.(com|net|es|gal|org)$/;
+let patronUsuario =/^[\w]{1,20}$/;
+let patronClave =/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+let patronTelefono =/\d{9}$/;
+
+function validarNombre(nombre){
+    if(!patronNombre.test(nombre)){
+        console.log("¡Sólo caracteres alfabéticos!");
+        return false;
+    }
+    return true;
+}
+
+function validarApellidos(apellidos){
+    if(!patronApellido.test(apellidos)){
+        console.log("¡Sólo caracteres alfabéticos!");
+        return false;
+    }
+    return true;
+}
+
+function validarCorreo(correo){
+    if(!patronCorreo.test(correo)){
+        console.log("Formato de email incorrecto. Sólo caracteres alfanuméricos antes y después del símbolo @ y terminar en .com|.net|.es|.gal|.org.");
+        return false;
+    }
+    return true;
+}
+
+function validarUsuario(usuario){
+    if(!patronUsuario.test(usuario)){
+        console.log("¡Sólo letras y números!");
+        return false;
+    }
+    return true;
+}
+
+function validarClave(clave){
+    if(!patronClave.test(clave)){
+        console.log("La clave debe contener al menos 8 caracteres, incluyendo al menos una letra mayúscula, una letra minúscula, un número y un carácter especial.");
+        return false;
+    }
+    return true;
+}
+
+function validarTelefono(tlf){
+    if(!patronTelefono.test(tlf)){
+        console.log("Longitud máxima 9 números.");
+        return false;
+    }
+    return true;
+}
+
+
+function validarFormulario(){
+    let nombre = document.getElementById("nombre").value;
+    let apellidos = document.getElementById("apellidos").value;
+    let correo = document.getElementById("correo").value;
+    let usuario = document.getElementById("usuario").value;
+    let clave = document.getElementById("clave").value;
+    let telefono = document.getElementById("telefono").value;
+
+    if(
+        validarNombre(nombre) && validarApellidos(apellidos) && validarCorreo(correo) &&
+        validarUsuario(usuario) && validarClave(clave) && validarTelefono(telefono)
+    ){
+        return true;
+    }
+    return false;
+}
+
+
+
+// Envio de datos con XMLHttpRequest
+function sendData(){
+    let peticion = new XMLHttpRequest();
+    let data = new FormData(document.getElementById("formulario"));
+
+    peticion.open("POST", "php/registrar.php", true);
+    peticion.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    peticion.onreadystatechange = function(){
+        if(peticion.readyState === XMLHttpRequest.DONE){
+            if(peticion.status === 200){
+                document.getElementById("info-respuesta").style.display = "block";
+                document.getElementById("respuesta-servidor").innerHTML = peticion.responseText;
+            }else{
+                alert("No se han podido envíar los datos.")
+            }
+        }
+    };
+
+    peticion.send(
+        "nombre=" + data.get("nombre") + "&" +
+        "apellidos=" + data.get("apellidos") + "&" +
+        "telefono=" + data.get("telefono") + "&" +
+        "correo=" + data.get("correo") + "&" +
+        "usuario=" + data.get("usuario") + "&" +
+        "clave=" + data.get("clave")
+    );
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("formulario").addEventListener("submit", function(event){
+        event.preventDefault();
+        if(validarFormulario()){
+            sendData();
+        }else{
+            alert("Ha ocurrido un error. Formulario incorrecto.");
+        }
+    });
+});
